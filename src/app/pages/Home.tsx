e96b7d9881271031629ca1bc8b59011bb0b70f9f
aaul <aaul@uwo.ca>
2026-03-09 00:31:37 -0400

    Update Create Wordlist Dialog design.

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -113,14 +113,19 @@ export function Home() {
                 </div>
                 <div>
                   <Label htmlFor="words">Words (separated by commas)</Label>
-                  <Input
+                  <textarea
                     id="words"
                     placeholder="cat, dog, bird, fish"
                     value={customWords}
+                    draggable
+                    className="file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base bg-input-background transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm
+                      focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px],
+                      aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
+                    cols={60}
                     onChange={(e) => setCustomWords(e.target.value)}
                   />
                 </div>
-                <Button onClick={handleCreateCustomList} className="w-full">
+                <Button onClick={handleCreateCustomList} className="w-full hover:shadow-xl transition-all cursor-pointer border-2 hover:scale-105">
                   Create and Start
                 </Button>
               </div>
